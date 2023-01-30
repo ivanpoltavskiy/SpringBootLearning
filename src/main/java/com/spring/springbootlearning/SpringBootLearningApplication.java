0fc339455217a49043bf9cde48d58dfb13def118
d4ng3r228 <dangersof228@gmail.com>
2023-01-30 12:14:51 +0200

    Added DB1

--- src/main/java/com/spring/springbootlearning/SpringBootLearningApplication.java
+++ src/main/java/com/spring/springbootlearning/SpringBootLearningApplication.java
@@ -8,7 +8,7 @@ public class SpringBootLearningApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootLearningApplication.class, args);
-        System.out.println("Start");
+
     }
 
 }
