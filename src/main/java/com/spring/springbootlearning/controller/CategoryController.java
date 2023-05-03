c45f74f1cac1bf0e6fbf5813f8a4db8f89fc4d1a
d4ng3r228 <dangersof228@gmail.com>
2023-05-03 11:23:00 +0300

    PutMapping for category and priority

--- src/main/java/com/spring/springbootlearning/controller/CategoryController.java
+++ src/main/java/com/spring/springbootlearning/controller/CategoryController.java
@@ -1,8 +1,11 @@
 package com.spring.springbootlearning.controller;
 
 import com.spring.springbootlearning.entity.Category;
+import com.spring.springbootlearning.entity.Priority;
 import com.spring.springbootlearning.repository.CategoryRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,9 +22,25 @@ public class CategoryController {
     }
 
     @PostMapping("/add")
-    public Category add (@RequestBody Category category){
-        return categoryRepository.save(category);
+    public ResponseEntity<Category> add (@RequestBody Category category){
+        if (category.getId() !=null && category.getId() != 0){
+            return new ResponseEntity("ID should be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        if (category.getTitle() == null || category.getTitle().trim().length() == 0){
+            return new ResponseEntity("Title cannot be null", HttpStatus. NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(categoryRepository.save(category));
     }
 
+    @PutMapping("/update")
+    public ResponseEntity<Category> update (@RequestBody Category category){
+        if (category.getId() ==null && category.getId() == 0){
+            return new ResponseEntity("ID should not be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        if (category.getTitle() == null || category.getTitle().trim().length() == 0){
+            return new ResponseEntity("Title cannot be null", HttpStatus. NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(categoryRepository.save(category));
+    }
 
 }
--- src/main/java/com/spring/springbootlearning/controller/PriorityController.java
+++ src/main/java/com/spring/springbootlearning/controller/PriorityController.java
@@ -4,6 +4,8 @@ import com.spring.springbootlearning.entity.Category;
 import com.spring.springbootlearning.entity.Priority;
 import com.spring.springbootlearning.repository.PriorityRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,7 +23,27 @@ public class PriorityController {
     }
 
     @PostMapping("/add")
-    public Priority add (@RequestBody Priority priority){
-        return priorityRepository.save(priority);
+    public ResponseEntity<Priority> add (@RequestBody Priority priority){
+        if (priority.getId() !=null && priority.getId() != 0){
+            return new ResponseEntity("ID should be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0){
+            return new ResponseEntity("Title cannot be null", HttpStatus. NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(priorityRepository.save(priority));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Priority> update (@RequestBody Priority priority){
+        if (priority.getId() ==null && priority.getId() == 0){
+            return new ResponseEntity("ID should not be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0){
+            return new ResponseEntity("Title cannot be null", HttpStatus. NOT_ACCEPTABLE);
+        }
+        if (priority.getColor() == null || priority.getColor().trim().length() == 0){
+            return new ResponseEntity("Color cannot be null", HttpStatus. NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(priorityRepository.save(priority));
     }
 }
